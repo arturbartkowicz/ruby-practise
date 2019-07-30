4a16e40c9a3e27f465c91d306c849bce49cfb4d5
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-07-30 20:13:01 +0000

    feat: Ruby Basic Team treehouse - store exercise

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,26 @@
-puts "Hello world"
+puts "Hello world!"
+
+# Ruby Basics - team tree house - exercise
+
+puts "Ruby basics - exercise:"
+
+def ask(question)
+  print question + " "
+  gets.chomp
+end
+
+def price(quantity)
+  if quantity >=100
+    price_per_unit = 8
+  elsif quantity >= 50 && quantity <100
+    price_per_unit = 9
+  else quantity < 50
+    price_per_unit = 10
+  end
+quantity*price_per_unit
+end
+puts "Welcome to the widget store!"
+answer = ask ('How many widgets are you ordering?')
+number = answer.to_i
+total = price(number)
+print "For #{number} widgets, your total is: $#{total} "
